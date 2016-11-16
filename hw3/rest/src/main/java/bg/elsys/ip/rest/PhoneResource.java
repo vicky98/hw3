476d4d86057d61444c9780f8d3f402cc6c4220b4
vicky98 <vikiga98@gmail.com>
2016-11-16 23:49:13 +0200

    A little change (still not working properly)

--- hw3/rest/src/main/java/bg/elsys/ip/rest/PhoneResource.java
+++ hw3/rest/src/main/java/bg/elsys/ip/rest/PhoneResource.java
@@ -3,12 +3,14 @@ package bg.elsys.ip.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 @Path("/phones")
 public class PhoneResource {
@@ -32,7 +34,10 @@ public class PhoneResource {
 	
 	@POST
 	@Produces(MediaType.APPLICATION_JSON)
-	public void addPhone(Phone phone) {
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response addPhone(Phone phone) {
 		phones.add(phone);
+		
+		return Response.ok(phone).status(Status.CREATED).build();
 	}
 }
