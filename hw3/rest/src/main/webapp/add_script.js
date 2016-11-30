d53787b13c7c8023d6036c6e23799b4667af9b4a
vicky98 <vikiga98@gmail.com>
2016-11-30 23:24:25 +0200

    A little change but it still doesn't work

--- hw3/rest/src/main/webapp/add_script.js
+++ hw3/rest/src/main/webapp/add_script.js
@@ -1,18 +1,14 @@
 function addPhone() {
-	$.ajax({
+	var phone = '{ "imageURL":"' + $("#imageAdd").val() + '", "manufacturer":"' + $("#manuAdd").val() + '", "model": "' + $("#modelAdd").val() + '", "color": "' + $("#colorAdd").val() + '", "year": "' + $("#yearAdd").val() + '", "description": "' + $("#descAdd").val() + '"}';
+	
+	$.ajax({	
 		
 		url: "http://localhost:8080/rest/api/phones",
 		type: "POST",
+		data: phone,
 		dataType: "json",
 		success: function(data) {
 			console.log(data);
-			
-			var image = $('#imageAdd'.val());
-			var manu = $('#manuAdd'.val());
-			var model = $('#modelAdd'.val());
-			var color = $('#colorAdd'.val());
-			var year = $('#yearAdd'.val());
-			var desc = $('#descAdd'.val());
 		}
 	});
 }
